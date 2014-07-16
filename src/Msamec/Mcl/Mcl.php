381d513ccd6d5d94397731e91cec94bb2af31223
Marko Samec <marko.schamec@gmail.com>
2014-07-16 22:35:07 +0200

    Call for different methods depending on role

--- src/Msamec/Mcl/Mcl.php
+++ src/Msamec/Mcl/Mcl.php
@@ -1,7 +1,22 @@
 <?php namespace Msamec\Mcl;
 
 class Mcl{
-	public static function testit(){
-		echo 'it is work';
+	public function model($namespace, $function, $role = 'Main'){
+		if(class_exists($namespace.'\\'.$role))
+			$class_name = $namespace.'\\'.$role;
+		elseif(class_exists($namespace.'\\Main'))
+			$class_name = $namespace.'\\Main';
+		else
+			throw new \Exception("Namespace {$namespace} does not exist.");
+			
+
+		$class = new $class_name;
+		$arguments = array_slice(func_get_args(), 3);
+
+		$method = new \ReflectionMethod($class, $function);
+		if($arguments)
+			$method->invokeArgs($class, $arguments);
+		else
+			$method->invoke($class);
 	}
 }
