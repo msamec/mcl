d31d46b709ec1798788f2900109b62f66af20aff
Marko Samec <marko.schamec@gmail.com>
2014-07-16 23:28:57 +0200

    Bugfix: wrong namespace

--- src/Msamec/Mcl/Mcl.php
+++ src/Msamec/Mcl/Mcl.php
@@ -1,16 +1,17 @@
 <?php namespace Msamec\Mcl;
 
 class Mcl{
-	public function model($namespace, $function, $role = 'Main'){
+	public function model($namespace, $function, $role = null){
+		$defaultRoleName = \Config::get('mcl::default');
 		if(class_exists($namespace.'\\'.$role))
-			$class_name = $namespace.'\\'.$role;
-		elseif(class_exists($namespace.'\\Main'))
-			$class_name = $namespace.'\\Main';
+			$className = $namespace.'\\'.$role;
+		elseif(class_exists($namespace.'\\'.$defaultRoleName))
+			$className = $namespace.'\\'.$defaultRoleName;
 		else
 			throw new \Exception("Namespace {$namespace} does not exist.");
 			
 
-		$class = new $class_name;
+		$class = new $className;
 		$arguments = array_slice(func_get_args(), 3);
 
 		$method = new \ReflectionMethod($class, $function);
@@ -20,15 +21,14 @@ class Mcl{
 			$method->invoke($class);
 	}
 
-	public function make($view, $role = 'main', $data = array(), $mergeData = array())
+	public function make($view, $role = null, $data = array(), $mergeData = array())
 	{
 		$role = strtolower($role);
 		try{
-			\View::make($view.'.'.$role);
-		}catch(InvalidArgumentException $ex){
-			$role = 'main';
-			\View::make($view.'.'.$role);
+			return \View::make($view.'.'.$role, $data, $mergeData);
+		}catch(\InvalidArgumentException $ex){
+			$role = \Config::get('mcl::view');
+			return \View::make($view.'.'.$role, $data, $mergeData);
 		}
-		return \View::make($view.'.'.$role, $data, $mergeData);
 	}
 }
